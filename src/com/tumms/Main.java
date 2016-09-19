4d53f45370b93b538d5939d4eb964b9ab05732d7
jtumms <john.tumminelli@tummsmedia.com>
2016-09-19 11:33:38 -0400

    multi user

--- src/com/tumms/Main.java
+++ src/com/tumms/Main.java
@@ -1,52 +1,71 @@
 package com.tumms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        ArrayList<Item> items = new ArrayList<>();
+
+
+        HashMap<String, ArrayList<Item>> users = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("1. Create to-do item");
-            System.out.println("2 Check/Uncheck to-do item");
-            System.out.println("3. List all to-do items");
+            System.out.println("Enter name:");
+            String name = scanner.nextLine();
 
-            String option = scanner.nextLine();
+            ArrayList<Item> items = users.get(name);
+            if (items == null){
+                items = new ArrayList<>();
+                users.put(name, items);
+            }
 
-            switch (option) {
-                case "1":
-                    System.out.println("Enter your to-to item");
-                    String text = scanner.nextLine();
-                    Item item = new Item(text, false);
-                    items.add(item);
-                    break;
-                case "2":
-                    System.out.println("Which item do you want to toggle?");
-                    int i = Integer.valueOf(scanner.nextLine());
-                    Item item2 = items.get(i-1);
-                    item2.isDone = !item2.isDone;
-                    break;
-                case "3":
-                    for (int j = 0; j <items.size(); j++) {
-                        Item item3 = items.get(j);
-                        int num = j+1;
-                        String checkbox = "[ ]";
-                        if (item3.isDone) {
-                            checkbox = "[x]";
+            boolean isLoggedIn = true;
+            while (isLoggedIn) {
+                System.out.println("1. Create to-do item");
+                System.out.println("2 Check/Uncheck to-do item");
+                System.out.println("3. List all to-do items");
+                System.out.println("4. Logout");
+
+                String option = scanner.nextLine();
+
+                switch (option) {
+                    case "1":
+                        System.out.println("Enter your to-to item");
+                        String text = scanner.nextLine();
+                        Item item = new Item(text, false);
+                        items.add(item);
+                        break;
+                    case "2":
+                        System.out.println("Which item do you want to toggle?");
+                        int i = Integer.valueOf(scanner.nextLine());
+                        Item item2 = items.get(i - 1);
+                        item2.isDone = !item2.isDone;
+                        break;
+                    case "3":
+                        for (int j = 0; j < items.size(); j++) {
+                            Item item3 = items.get(j);
+                            int num = j + 1;
+                            String checkbox = "[ ]";
+                            if (item3.isDone) {
+                                checkbox = "[x]";
+                            }
+
+                            System.out.println(checkbox + " " + num + ". " + item3.text);
+
                         }
+                        break;
+                    case "4":
+                        isLoggedIn = false;
+                        break;
 
-                        System.out.println(checkbox + " " + num + ". " + item3.text);
-
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid option.");
+                    default:
+                        System.out.println("Invalid option.");
 
 
+                }
             }
         }
 
