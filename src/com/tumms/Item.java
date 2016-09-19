ef62f27dc11111abc3a2e2de8015f0dfabb66b6b
jtumms <john.tumminelli@tummsmedia.com>
2016-09-19 11:16:58 -0400

    Add

---
+++ src/com/tumms/Item.java
@@ -0,0 +1,14 @@
+package com.tumms;
+
+/**
+ * Created by john.tumminelli on 9/19/16.
+ */
+public class Item {
+    String text;
+    boolean isDone;
+
+    public Item(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
