5cdeee8d33c75db5c00a722cf457a71f286981bc
jtumms <john.tumminelli@tummsmedia.com>
2016-10-12 11:43:15 -0400

    Add H2 database

--- src/com/tumms/Item.java
+++ src/com/tumms/Item.java
@@ -4,10 +4,12 @@ package com.tumms;
  * Created by john.tumminelli on 9/19/16.
  */
 public class Item {
+    int id;
     String text;
     boolean isDone;
 
-    public Item(String text, boolean isDone) {
+    public Item(int id, String text, boolean isDone) {
+        this.id = this.id;
         this.text = text;
         this.isDone = isDone;
     }
